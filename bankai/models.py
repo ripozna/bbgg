7015cab2aab223025d41f452311d5fe9cadbe528
ripozna <ov455379@gmail.com>
2021-10-22 18:07:43 +0600

    fff

--- bankai/models.py
+++ bankai/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}-{self.surname}'
 # Create your models here.
